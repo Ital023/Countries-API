be00e88e00ccb58dd182ba4a748dfb91f4bfc482
Ital023 <contadoitalo911@gmail.com>
2024-08-26 18:25:57 -0300

    feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
-import darkModeIcon from "../../assets/DarkModeIcon.svg"
+import darkModeIcon from "../../assets/DarkModeIcon.svg";
 
 export default function Header() {
-    return(
-        <header className="bg-white h-20">
-            <h1>Where in the world?</h1>
+  return (
+    <header className="bg-white h-20 flex justify-center content-center shadow-sm">
+      <div className="container px-4 flex flex-row items-center justify-between h-full">
+        <h1 className="font-extrabold text-sm md:text-2xl text-lightMode-VeryDarkBlue">
+          Where in the world?
+        </h1>
 
-            <div>
-                <img src={darkModeIcon} alt="" />
-            </div>
-        </header>
-    );
+        <div className="flex flex-row justify-between items-center gap-2">
+          <img src={darkModeIcon} alt="Dark mode icon" className="w-4 md:w-5 md:h-5 h-4" />
+          <p className="font-semibold text-xs md:text-base text-lightMode-VeryDarkBlue">
+            Dark Mode
+          </p>
+        </div>
+      </div>
+    </header>
+  );
 }
