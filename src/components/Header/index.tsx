1da544fd55e906b342c328acb15dca10bf009247
Ital023 <contadoitalo911@gmail.com>
2024-08-27 23:40:28 -0300

    feat: header link to home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [dark, setDark] = useState<boolean>(false);
@@ -13,20 +14,21 @@ export default function Header() {
   return (
     <header className="bg-lightMode-quaternary h-20 flex justify-center content-center shadow-sm dark:bg-darkMode-tertiary">
       <div className="container px-4 flex flex-row items-center justify-between h-full">
-        <h1 className="font-extrabold text-sm md:text-2xl text-lightMode-tertiary dark:text-darkMode-secondary">
-          Where in the world?
-        </h1>
-
+        <Link to={"/"}>
+          <h1 className="font-extrabold text-sm md:text-2xl text-lightMode-tertiary dark:text-darkMode-secondary">
+            Where in the world?
+          </h1>
+        </Link>
         <div
           className="flex flex-row justify-between items-center gap-2 cursor-pointer"
           onClick={darkModeHandler}
         >
-          {dark && 
+          {dark && (
             <FontAwesomeIcon icon={faMoon} style={{ color: "#ffffff" }} />
-          }
-          {!dark && 
+          )}
+          {!dark && (
             <FontAwesomeIcon icon={faMoon} style={{ color: "#000000" }} />
-          }
+          )}
 
           <p className="font-semibold text-xs md:text-base text-lightMode-tertiary dark:text-darkMode-secondary">
             Dark Mode
