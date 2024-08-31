54f2dd72369cd3500c155ef7dfb0577f04ed0809
Ital023 <contadoitalo911@gmail.com>
2024-08-31 11:51:43 -0300

    feat: Link Border Country

--- src/components/BorderCountryCard/index.tsx
+++ src/components/BorderCountryCard/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import * as countryService from "../../services/country-service";
 import { useParams } from "react-router-dom";
 import { BorderCountryNameDTO } from "../../models/BorderCountryNameDTO";
+import { Link } from "react-router-dom";
 
 
 type Props = {
@@ -27,9 +28,10 @@ export default function BorderCountryCard({ id }: Props) {
 
 
     return(
+        <Link to={`/countries/${id}`}>
         <button className="w-24 py-1.5  rounded-sm shadow-md bg-lightMode-quaternary dark:bg-darkMode-tertiary">
-            <p className="invisible">{id}</p>
             <p className="text-lightMode-tertiary text-xs font-medium dark:text-darkMode-secondary">{countryName}</p>
         </button>
+        </Link>
     );
 }
